6d89b8100180d6f1ce887d42a02055690fb47bef
aaltwaim <49359516+aaltwaim@users.noreply.github.com>
2019-12-31 10:39:14 +0300

    Solution

---
+++ main.js
@@ -0,0 +1,21 @@
+class Calculator{
+    constructor(){
+        this.range =[];
+        this.primes =[];
+    }
+    getPrime(endNum){
+        for(let i =2; i<=endNum;i++){
+            this.range.push(i);
+        }
+        while(this.range.length>0){
+            let nextPrime = this.range.shift();
+            this.primes.push(nextPrime);
+            this.range = this.range.filter(function (num){
+                return num % nextPrime !== 0;
+            })
+        }
+console.log(`prime number ${this.primes}`);
+    }
+}
+let test = new Calculator();
+test.getPrime(50)
